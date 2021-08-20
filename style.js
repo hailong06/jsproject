8d794149b4015f0cd41fff5e36b22b77d4db6fcf
Hailong0962 <hailong0962@gmail.com>
2021-08-20 17:48:53 +0700

    Update chuc nang dang ki dang nhap logout

--- style.js
+++ style.js
@@ -2,11 +2,7 @@ const signinBtn = document.querySelector('.signinBtn');
 const signupBtn = document.querySelector('.signupBtn');
 const formBx = document.querySelector('.formBx');
 const body = document.querySelector('body');
-// const username = document.getElementById('.usename');
-// const password = document.getElementById('.password');
-// const form = document.getElementById('form');
-// const errorElement = document.getElementById('error');
-
+const form = document.getElementById('#form');
 
 signupBtn.onclick = function(){
     formBx.classList.add('active');
@@ -16,29 +12,6 @@ signinBtn.onclick = function(){
     formBx.classList.remove('active');
     body.classList.remove('active');
 }
-// function alertInformation() {
-//     var username = 'admin';
-//     var password = '123456';
-
-//     var userName = document.getElementById('username').value;
-//     var passWord = document.getElementById('password').value;
-
-//     if ((username == userName) && (password == passWord)) {
-//         swal("Good job", "Login Success");
-//     } else {
-//         sweetAlert("Oops...", "Username or Password was wrong");
-//     }
-//   }
-// form.addEventListener('submit', (e) => {
-//     let messenger = [];
-//     if (password.value.length <= 6) {
-//         messenger.push("Password must be longer than 6 characters")
-//     }
-//     if (messenger.length > 0){
-//         e.preventDefault();
-//         errorElement.innerText = messenger.join(', ');
-//     }
-// })
 var objPeople = [
 	{ // Object @ 0 index
 		username: "admin",
@@ -48,14 +21,75 @@ var objPeople = [
 function getInfo() {
 	var username = document.getElementById('username').value
 	var password = document.getElementById('password').value
-
+	var success = -1;
 	for(var i = 0; i < objPeople.length; i++) {
 		// check is user input matches username and password of a current index of the objPeople array
 		if(username == objPeople[i].username && password == objPeople[i].password) {
-			alert(username + " is logged in!!!")
-			// stop the function if this is found to be true
+			document.getElementById("inforname1").style.display= 'none';
+			document.getElementById("infor").style.display= 'block';
+			document.getElementById("logout").style.display= 'block';
+			console.log(infor);
+			alert("Welcome " + username + " is logged in!!!")
+			success = i;
+			prname = username;
+			document.getElementById("inforname").innerHTML= "Welcome " + prname;
 			return
+		}else if(username == '' || password == ''){
+			alert("Không để trống");
 		}
+		
 	}
-	alert("incorrect username or password")
+	alert("Sai");
 }
+function registerUser(){
+    var registerUser = document.getElementById("newUsername").value
+    var registerPassword = document.getElementById("newPassword").value
+	var Repassword = document.getElementById("Repassword").value
+    var newUser = {
+        username: registerUser,
+        password: registerPassword,
+		repassword: Repassword
+    }
+    for (i = 0; i < objPeople.length; i++) {
+        if(registerUser == objPeople[i].username){
+            alert("that username is already use, please choose another")
+            return
+        } else if(registerPassword.length<6){
+            alert("password is too short, include 6 or more characters")
+        }else if(registerPassword === Repassword ){
+            alert("password and repassword  không trùng khớp")
+        }
+    }
+    objPeople.push(newUser)
+    console.log(objPeople)
+}
+function Logout() {
+    document.getElementById("inforname1").style.display = 'block';
+    document.getElementById("infor").style.display = 'none';
+    document.getElementById("logout").style.display = 'none';
+	document.getElementById("inforname").style.display = 'none';
+    document.getElementsById("formlogin").reset();
+}
+// function alertInformation() {
+//     var username = 'admin';
+//     var password = '123456';
+
+//     var userName = document.getElementById('username').value;
+//     var passWord = document.getElementById('password').value;
+
+// if ((username == userName) && (password == passWord)) {
+// 	swal("Good job", "Login Success");
+// } else {
+// 	sweetAlert("Oops...", "Username or Password was wrong");
+// }
+// }
+// form.addEventListener('submit', (e) => {
+// if (password.value.length <= 6) {
+// 	alert("Password must be longer than 6 characters");
+// }
+// if(password.value.length < 6) {
+// 	alert( "Password must be longer than 6 characters")
+// 	// stop the function if this is found to be true
+// 	return
+// }
+// })
